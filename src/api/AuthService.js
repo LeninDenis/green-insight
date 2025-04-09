b6b8ed71eabf7e81bbc71536a8d4ba12a13d7af3
LeninDenis <lenin.denis@mail.ru>
2025-04-09 23:32:23 +0500

    micro bug fix

--- src/api/AuthService.js
+++ src/api/AuthService.js
@@ -1,27 +1,24 @@
-import { Buffer } from 'buffer';
-import {baseAxios} from "./axiosInstance";
+import { baseAxios } from "./axiosInstance";
 
-export default class AuthService{
-    static async login(userData){
-        const authString = Buffer
-            .from(`${userData.email}:${userData.password}`)
-            .toString('base64');
+export default class AuthService {
+    static async login(userData) {
+        const authString = btoa(`${userData.email}:${userData.password}`);
         const headers = {
             Authorization: `Basic ${authString}`
         };
         const response = await baseAxios
-            .post('http://localhost:8080/auth/login',null, {headers});
+            .post('http://localhost:8080/auth/login', null, { headers });
         return response;
     }
 
     // {"fname": "", "lname": "", "email": "", "password": ""}
-    static async register(newUserForm){
+    static async register(newUserForm) {
         const response = await baseAxios
             .post('http://localhost:8080/auth/register', newUserForm);
         return response;
     }
 
-    static async refresh(refreshToken){
+    static async refresh(refreshToken) {
         const body = {
             token: refreshToken,
             tokenType: "REFRESH"
